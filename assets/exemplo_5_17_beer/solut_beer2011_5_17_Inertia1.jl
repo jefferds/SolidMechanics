5a5516207d2cc7c24a1be49e3e781eb2096e5abf
jefferds <jefferds@gmail.com>
2022-05-24 19:21:13 +0000

    Deploying to gh-pages from  @ 3ed9900b4819eb186cad606d4f67a744775a63ec 🚀

--- assets/exemplo_5_17_beer/solut_beer2011_5_17_Inertia1.jl
+++ assets/exemplo_5_17_beer/solut_beer2011_5_17_Inertia1.jl
@@ -3,12 +3,12 @@
 
 h = 420.0e-3
 bf = 261.0e-3
-tf = 11.6e-3
-tw = 19.3e-3
+tw = 11.6e-3
+tf = 19.3e-3
 d1 = d3 = h/2-tf/2
 
 I1z = (bf * tf^3)/12 + bf*tf*d1^2
-I2z = (tf * (h-2*tf)^3)/12
+I2z = (tw * (h-2*tf)^3)/12
 I3z = (bf * tf^3)/12 + bf*tf*d3^2
 
 Iz = I1z + I2z + I3z
